39080204914a941a7657da903c03f034e5f5f2df
caleb <caleb@cedarrobota.com>
2020-01-10 23:29:45 -0800

    modularized employee code

---
+++ Cedar/employee/qsr_employee.py
@@ -0,0 +1,191 @@
+import datetime
+import json
+import smtplib
+import sys
+import time
+import uuid
+import plivo
+import os
+import firebase_admin
+from passlib.hash import pbkdf2_sha256
+from firebase_admin import credentials
+from firebase_admin import db
+from flask import Blueprint, render_template, abort
+from google.cloud import storage
+import pytz
+from flask import Flask, flash, request, session, jsonify
+from werkzeug.utils import secure_filename
+from flask import redirect, url_for
+from flask import render_template
+from flask_session import Session
+from flask_sslify import SSLify
+from square.client import Client
+from werkzeug.datastructures import ImmutableOrderedMultiDict
+from flask import Blueprint, render_template, abort
+from Cedar.collect_menu import findMenu
+from Cedar.admin import admin_panel
+from Cedar.admin.admin_panel import checkLocation, sendEmail, getSquare
+
+
+
+qsr_employee_blueprint = Blueprint('qsr_employee', __name__,template_folder='templates')
+
+mainLink = 'https://033d08d3.ngrok.io/'
+
+
+
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-employee-login')
+def EmployeeLoginQSR(estNameStr,location):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    return(render_template("POS/StaffQSR/login.html"))
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-employee-login2')
+def EmployeeLogin2QSR(estNameStr,location):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    return(render_template("POS/StaffSitdown/login2.html"))
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-employee-login', methods=['POST'])
+def EmployeeLoginCheckQSR(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    code = rsp['code']
+    loginRef = db.reference('/restaurants/' + estNameStr + '/' + location + "/employee")
+    loginData = dict(loginRef.get())
+    hashCheck = pbkdf2_sha256.verify(code, loginData['code'])
+    if(hashCheck == True):
+        token = str(uuid.uuid4())
+        loginRef.update({
+            "token":token,
+            "time":time.time()
+        })
+        session["token"] = token
+        return(redirect(url_for("EmployeePanelQSR",estNameStr=estNameStr,location=location)))
+    else:
+        return(redirect(url_for("EmployeeLogin2",estNameStr=estNameStr,location=location)))
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-view')
+def EmployeePanelQSR(estNameStr,location):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    token = session.get('token',None)
+    loginRef = db.reference('/restaurants/' + estNameStr + '/' + location + "/employee/")
+    loginData = dict(loginRef.get())
+    try:
+        if(((token == loginData["token"]) and (time.time() - loginData["time"] <= 3600))):
+            pass
+        else:
+            return(redirect(url_for("EmployeeLoginQSR",estNameStr=estNameStr,location=location)))
+    except Exception as e:
+        return(redirect(url_for("EmployeeLogin2QSR",estNameStr=estNameStr,location=location)))
+    try:
+        ordPath = '/restaurants/' + estNameStr + '/' + location + "/orderQSR"
+        ordsRef = db.reference(ordPath)
+        ordsGet = dict(ordsRef.get())
+    except Exception as e:
+        ordsGet = {}
+    menu = findMenu(estNameStr,location)
+    pathMenu = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories"
+    menuInfo = dict(db.reference(pathMenu).get())
+    categories = list(menuInfo.keys())
+    activeItems = []
+    inactiveItems = []
+    for cats in categories:
+        itms = list(dict(menuInfo[cats]).keys())
+        for itm in itms:
+            if(menuInfo[cats][itm]["descrip"] != "INACTIVE"):
+                appItem = str(cats) + "~" + str(itm)
+                activeItems.append(appItem)
+            else:
+                appItem = str(cats) + "~" + str(itm)
+                inactiveItems.append(appItem)
+    return(render_template("POS/StaffQSR/View.html", location=location.capitalize(), restName=str(estNameStr.capitalize()), menu=menu, activeItems=activeItems, inactiveItems=inactiveItems, orders=ordsGet))
+
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-activate-item~<cat>~<item>~<menu>')
+def activateItemQSR(estNameStr,location,cat,item,menu):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    pathMenu = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories/"+ cat + "/" + item
+    descrip = dict(db.reference(pathMenu).get())["tmp"]
+    db.reference(pathMenu).update({"descrip":descrip})
+    pathDel = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories/"+ cat + "/" + item +"/tmp"
+    db.reference(pathDel).delete()
+    return(redirect(url_for("EmployeePanelQSR",estNameStr=estNameStr,location=location)))
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-deactivate-item~<cat>~<item>~<menu>')
+def deactivateItemQSR(estNameStr,location,cat,item,menu):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    pathMenu = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories/"+ cat + "/" + item
+    descrip = dict(db.reference(pathMenu).get())["descrip"]
+    db.reference(pathMenu).update({"tmp":descrip})
+    db.reference(pathMenu).update({"descrip":"INACTIVE"})
+    return(redirect(url_for("EmployeePanelQSR",estNameStr=estNameStr,location=location)))
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-view-success', methods=["POST"])
+def EmployeeSuccessQSR(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    reqToken = rsp["req"]
+    pathRequest = '/restaurants/' + estNameStr + '/' + location + "/orderQSR/" + reqToken
+    reqRef = db.reference(pathRequest)
+    reqRef.delete()
+    return(redirect(url_for("EmployeePanelQSR",estNameStr=estNameStr,location=location)))
+
+@qsr_employee_blueprint.route('/<estNameStr>/<location>/qsr-sendOrder', methods=["POST"])
+def sendOrderQsr(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    token = rsp["token"]
+    pathOrder = '/restaurants/' + estNameStr + '/' + location + "/orders/" + token
+    orderRef = db.reference(pathOrder)
+    order = dict(orderRef.get())
+    qsrOrderPath = '/restaurants/' + estNameStr + '/' + location + '/orderQSR'
+    qsrOrderRef = db.reference(qsrOrderPath)
+    qsrOrderRef.update({
+        token:{
+            "cart":dict(order['cart']),
+            "info":{"name":order["name"],
+                    "number":order['phone'],
+                    "paid":"PAID",
+                    "subtotal":order['subtotal'],
+                    "total":order['total'],
+                    'table':order['table']}
+            }
+    })
+
+    checkmate = db.reference('/restaurants/' + estNameStr + '/' + location + '/checkmate').get()
+    if(checkmate == 0):
+        sendCheckmate(estNameStr,location, token)
+
+    if(order['email'] != "no-email"):
+        getSquare(estNameStr)
+        now = datetime.datetime.now(tzGl[location])
+        write_str = "Your Order From "+ estNameStr.capitalize()  + " " + location.capitalize() + " @"
+        write_str += str(now.hour) + ":" + str(now.minute) + " on " + str(now.month) + "-" + str(now.day) + "-" + str(now.year)
+        write_str += "\n \n"
+        cart = dict(order["cart"])
+        subtotal = 0
+        items = []
+        cartKeys = list(cart.keys())
+        for keys in cartKeys:
+            subtotal += cart[keys]["price"]
+            dispStr = cart[keys]["dispStr"]
+            write_str += dispStr + "\n"
+        subtotal += 0.25
+        subtotalStr = "${:0,.2f}".format(subtotal)
+        taxRate = float(db.reference('/restaurants/' + estNameStr + '/' + location + '/taxrate').get())
+        tax = "${:0,.2f}".format(subtotal * taxRate)
+        tax += " (" + str(float(taxRate * 100)) + "%)"
+        total = "${:0,.2f}".format(subtotal * (1+taxRate))
+        write_str+= "\n \n"
+        write_str += subtotalStr +"\n"+tax + "\n"+ total +"\n \n \n"
+        write_str += 'Thank You For Your Order ' + str(order['name']).capitalize() + " !"
+        SUBJECT = "Your Order From "+ estNameStr.capitalize()  + " " + location.capitalize()
+        message = 'Subject: {}\n\n{}'.format(SUBJECT, write_str)
+        # smtpObj.sendmail(sender, [order['email']], message)
+        sendEmail(sender, order['email'], mesg)
+    return(redirect(url_for("EmployeePanelQSR",estNameStr=estNameStr,location=location)))
---
+++ Cedar/employee/sd_employee.py
@@ -0,0 +1,251 @@
+import datetime
+import json
+import smtplib
+import sys
+import time
+import uuid
+import plivo
+import os
+import firebase_admin
+from passlib.hash import pbkdf2_sha256
+from firebase_admin import credentials
+from firebase_admin import db
+from flask import Blueprint, render_template, abort
+from google.cloud import storage
+import pytz
+from flask import Flask, flash, request, session, jsonify
+from werkzeug.utils import secure_filename
+from flask import redirect, url_for
+from flask import render_template
+from flask_session import Session
+from flask_sslify import SSLify
+from square.client import Client
+from werkzeug.datastructures import ImmutableOrderedMultiDict
+from flask import Blueprint, render_template, abort
+from Cedar.collect_menu import findMenu
+from Cedar.admin import admin_panel
+from Cedar.admin.admin_panel import checkLocation, sendEmail, getSquare
+
+
+
+sd_employee_blueprint = Blueprint('sd_employee', __name__,template_folder='templates')
+
+mainLink = 'https://033d08d3.ngrok.io/'
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/employee-login')
+def EmployeeLogin(estNameStr,location):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    return(render_template("POS/StaffSitdown/login.html"))
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/employee-login2')
+def EmployeeLogin2(estNameStr,location):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    return(render_template("POS/StaffSitdown/login2.html"))
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/employee-login', methods=['POST'])
+def EmployeeLoginCheck(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    code = rsp['code']
+    loginRef = db.reference('/restaurants/' + estNameStr + '/' + location + "/employee")
+    loginData = dict(loginRef.get())
+    hashCheck = pbkdf2_sha256.verify(code, loginData['code'])
+    if(hashCheck == True):
+        token = str(uuid.uuid4())
+        loginRef.update({
+            "token":token,
+            "time":time.time()
+        })
+        session["token"] = token
+        return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+    else:
+        return(redirect(url_for("EmployeeLogin2",estNameStr=estNameStr,location=location)))
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/view')
+def EmployeePanel(estNameStr,location):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    token = session.get('token',None)
+    loginRef = db.reference('/restaurants/' + estNameStr + '/' + location + "/employee/")
+    loginData = dict(loginRef.get())
+    try:
+        if(((token == loginData["token"]) and (time.time() - loginData["time"] <= 3600))):
+            pass
+        else:
+            return(redirect(url_for("EmployeeLogin",estNameStr=estNameStr,location=location)))
+    except Exception as e:
+        return(redirect(url_for("EmployeeLogin2",estNameStr=estNameStr,location=location)))
+    try:
+        ordPath = '/restaurants/' + estNameStr + '/' + location + "/orders/"
+        ordsRef = db.reference(ordPath)
+        ordsGet = dict(ordsRef.get())
+        tokens = list(ordsGet)
+        for tt in tokens:
+            if(int(ordsGet[tt]["QSR"]) == 0):
+                print(ordsGet[tt])
+                del ordsGet[tt]
+    except Exception as e:
+        ordsGet = {}
+    try:
+        pathRequest = '/restaurants/' + estNameStr + '/' + location + "/requests/"
+        reqRef = db.reference(pathRequest)
+        reqData = dict(reqRef.get())
+    except Exception as e:
+        reqData = {}
+    menu = findMenu(estNameStr,location)
+    pathMenu = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories"
+    menuInfo = dict(db.reference(pathMenu).get())
+    categories = list(menuInfo.keys())
+    activeItems = []
+    inactiveItems = []
+    for cats in categories:
+        itms = list(dict(menuInfo[cats]).keys())
+        for itm in itms:
+            if(menuInfo[cats][itm]["descrip"] != "INACTIVE"):
+                appItem = str(cats) + "~" + str(itm)
+                activeItems.append(appItem)
+            else:
+                appItem = str(cats) + "~" + str(itm)
+                inactiveItems.append(appItem)
+    return(render_template("POS/StaffSitdown/View.html", location=location.capitalize(), restName=str(estNameStr.capitalize()), menu=menu, activeItems=activeItems, inactiveItems=inactiveItems, reqData=reqData, orders=ordsGet))
+
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/activate-item~<cat>~<item>~<menu>')
+def activateItem(estNameStr,location,cat,item,menu):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    pathMenu = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories/"+ cat + "/" + item
+    descrip = dict(db.reference(pathMenu).get())["tmp"]
+    db.reference(pathMenu).update({"descrip":descrip})
+    pathDel = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories/"+ cat + "/" + item +"/tmp"
+    db.reference(pathDel).delete()
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/deactivate-item~<cat>~<item>~<menu>')
+def deactivateItem(estNameStr,location,cat,item,menu):
+    if(checkLocation(estNameStr,location) == 1):
+        return(redirect(url_for("findRestaurant")))
+    pathMenu = '/restaurants/' + estNameStr + '/' + location + "/menu/" + menu + "/categories/"+ cat + "/" + item
+    descrip = dict(db.reference(pathMenu).get())["descrip"]
+    db.reference(pathMenu).update({"tmp":descrip})
+    db.reference(pathMenu).update({"descrip":"INACTIVE"})
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/view-success', methods=["POST"])
+def EmployeeSuccess(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    reqToken = rsp["req"]
+    pathRequest = '/restaurants/' + estNameStr + '/' + location + "/requests/" + reqToken
+    reqRef = db.reference(pathRequest)
+    reqData = dict(reqRef.get())
+    orderToken = reqData["info"]["token"]
+    if(reqData["info"]["type"] == "order"):
+        pathTicket = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken +"/ticket"
+        pathTotal = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken
+        del reqData["info"]
+        tickRef = db.reference(pathTicket)
+        cartData = reqData
+        cartKeys = list(cartData.keys())
+        # return(str(cartKeys))
+        addTotal = 0
+        req = []
+        for ccx in range(len(cartKeys)):
+            addTotal += float(cartData[cartKeys[ccx]]["price"])
+        tickRef.push(reqData)
+        currTotal = float(dict(db.reference(pathTotal).get())["subtotal"])
+        tickTotal = db.reference(pathTotal).update({"subtotal":float(currTotal+addTotal)})
+    reqRef.delete()
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/view-warning', methods=["POST"])
+def EmployeeWarn(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    reqToken = rsp["req"]
+    alert = rsp["reason"]
+    pathRequest = '/restaurants/' + estNameStr + '/' + location + "/requests/" + reqToken
+    reqRef = db.reference(pathRequest)
+    reqData = dict(reqRef.get())
+    orderToken = reqData["info"]["token"]
+    pathRequest = '/restaurants/' + estNameStr + '/' + location + "/requests/" + reqToken
+    pathUser = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken
+    reqRef = db.reference(pathRequest)
+    reqData = dict(reqRef.get())
+    orderToken = reqData["info"]["token"]
+    if(reqData["info"]["type"] == "order"):
+        pathTicket = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken +"/ticket"
+        pathTotal = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken
+        del reqData["info"]
+        tickRef = db.reference(pathTicket)
+        cartData = reqData
+        cartKeys = list(cartData.keys())
+        # return(str(cartKeys))
+        addTotal = 0
+        req = []
+        for ccx in range(len(cartKeys)):
+            addTotal += float(cartData[cartKeys[ccx]]["price"])
+        tickRef.push(reqData)
+        currTotal = float(dict(db.reference(pathTotal).get())["subtotal"])
+        tickTotal = db.reference(pathTotal).update({"subtotal":float(currTotal+addTotal)})
+    AlertSend = db.reference(pathUser).update({"alert":str("Warning: "+alert)})
+    AlertTime = db.reference(pathUser).update({"alertTime":time.time()})
+    reqRef.delete()
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/view-reject', methods=["POST"])
+def EmployeeReject(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    reqToken = rsp["req"]
+    alert = rsp["reason"]
+    pathRequest = '/restaurants/' + estNameStr + '/' + location + "/requests/" + reqToken
+    reqRef = db.reference(pathRequest)
+    reqData = dict(reqRef.get())
+    orderToken = reqData["info"]["token"]
+    pathUser = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken
+    AlertSend = db.reference(pathUser).update({"alert":str("Request Cancelled: "+alert)})
+    AlertTime = db.reference(pathUser).update({"alertTime":time.time()})
+    reqRef.delete()
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/view-editBill', methods=["POST"])
+def EditBill(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    orderToken = rsp["req"]
+    amt = float(rsp["amt"])
+    itm = str(rsp["itm"])
+    pathUserX = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken
+    pathUser = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken + "/ticket"
+    cartRefItm = db.reference(pathUser)
+    subtotal = float(db.reference(pathUserX).get()["subtotal"])
+    subtotal += amt
+    db.reference(pathUserX).update({"subtotal":subtotal})
+    cartRefItm.push({str(uuid.uuid4()):{
+        'cat':"",
+        'img':"",
+        'itm':itm,
+        'qty':1,
+        'notes':"added by staff",
+        'dispStr':"Staff Correction: "+ itm + "  ($" + "{:0,.2f}".format(amt) + ")",
+        'price':amt,
+        'mods':[["",str(amt)]],
+        'unitPrice':amt
+    }})
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
+
+
+@sd_employee_blueprint.route('/<estNameStr>/<location>/view-clearTicket', methods=["POST"])
+def RemBill(estNameStr,location):
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    rsp = ((request.form))
+    orderToken = rsp["req"]
+    pathUserX = '/restaurants/' + estNameStr + '/' + location + "/orders/" + orderToken
+    remRef = db.reference(pathUserX)
+    remRef.delete()
+    return(redirect(url_for("EmployeePanel",estNameStr=estNameStr,location=location)))
